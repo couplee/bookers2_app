c677ee815f4dc63ab18e25ddeebc77d77fabaa66
couplee <couplee888@gmail.com>
2021-01-23 05:13:35 +0000

    [Finish] 4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '38da0e0a82344c0923f458d85c0b5db0bed3865cc2ddf80c90d99b6d81258b2350cf8c829628a8724f1f08a3eb6140348a073bf37ca75b66c997e3b59b927ea1'
